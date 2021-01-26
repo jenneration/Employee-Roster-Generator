60f54625cc5532a46d8563a5fe28d2f4d52658e5
Jenner Garcia <jenner.garcia@gmail.com>
2021-01-26 00:15:06 -0500

    styling done

--- app.js
+++ app.js
@@ -9,15 +9,14 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-// const { create } = require("domain");
+const teamMembers = [];
 
+//Regex variables for input validation
 const officeNumCk = /(?:\b|-)([0-9]{1,2}|100)\b/;
 const emailCk = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/;
 const idNumCk = /^[0-9]*$/;
 
-const teamMembers = [];
-
-
+//Initializing function that creates the team manager
 function createManager() {
     console.log("Hello! Let's build a team roster!\n"); // Add second line on a time interval and directly address the manager
     inquirer.prompt([{
@@ -71,11 +70,12 @@ function createManager() {
     ]).then(function(answers) {
         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
         teamMembers.push(manager);
-        console.log(teamMembers); //test
+        //console.log(teamMembers); 
         createTeamMembers();
     });
 };
 
+//Pivot function passed to each team member function. Asks if another team member is to be added or not.
 function createTeamMembers() {
     inquirer.prompt([{
         type: "list",
@@ -99,6 +99,7 @@ function createTeamMembers() {
     });
 };
 
+//Creates the Engineer(s) 
 function createEngineer() {
     console.log("Please enter information about this engineer.");
     inquirer.prompt([{
@@ -152,11 +153,12 @@ function createEngineer() {
     ]).then(function(answers) {
         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
         teamMembers.push(engineer);
-        console.log(teamMembers); //test
+        //console.log(teamMembers);
         createTeamMembers();
     });
 };
 
+//Creates the intern(s)
 function createIntern() {
     console.log("Please enter information about this intern.")
     inquirer.prompt([{
@@ -210,44 +212,23 @@ function createIntern() {
     ]).then(function(answers) {
         const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
         teamMembers.push(intern);
-        console.log(teamMembers); //test
+        //console.log(teamMembers); 
         createTeamMembers();
     });
 };
 
+//Renders the team roster.
 function renderTeamRoster() {
+    //Checks if an output folder exists and if not, creates one.
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR);
     }
-
+    //Calls the render function with teamMembers passed to write to an html file.
     fs.writeFile(outputPath, render(teamMembers), function(err) {
         if (err) throw err;
         console.log("Your employee roster has been generated!");
     });
 };
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
+//Initializes the createManager function in the terminal to being process of inputting info for  the team roster.
 createManager();
